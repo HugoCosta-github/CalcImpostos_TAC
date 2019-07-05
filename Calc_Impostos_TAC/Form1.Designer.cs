1078a684bbabc2c9873f1671ac02162bc09fbce2
hugonc <hugocosta.ti@gmail.com>
2019-07-05 16:21:57 -0300

    Calculadora de Impostos TAC desenvolvida em WindowsForms

---
+++ Calc_Impostos_TAC/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Calc_Impostos_TAC
+{
+    partial class frmCalcTAC
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalcTAC));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtValorFrete = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.lblINSS = new System.Windows.Forms.Label();
+            this.lblIRRF = new System.Windows.Forms.Label();
+            this.lblSEST_SENAT = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INSS:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IRRF:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "SEST/SENAT:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Insira o valor do Frete:";
+            // 
+            // txtValorFrete
+            // 
+            this.txtValorFrete.AllowDrop = true;
+            this.txtValorFrete.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtValorFrete.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtValorFrete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValorFrete.Location = new System.Drawing.Point(185, 21);
+            this.txtValorFrete.Name = "txtValorFrete";
+            this.txtValorFrete.Size = new System.Drawing.Size(140, 26);
+            this.txtValorFrete.TabIndex = 4;
+            this.txtValorFrete.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtValorFrete_KeyPress);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(337, 21);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(92, 35);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(337, 71);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(92, 35);
+            this.btnLimpar.TabIndex = 9;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(336, 232);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(92, 34);
+            this.btnFechar.TabIndex = 10;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // lblINSS
+            // 
+            this.lblINSS.AutoSize = true;
+            this.lblINSS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblINSS.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblINSS.Location = new System.Drawing.Point(61, 23);
+            this.lblINSS.Name = "lblINSS";
+            this.lblINSS.Size = new System.Drawing.Size(0, 20);
+            this.lblINSS.TabIndex = 11;
+            // 
+            // lblIRRF
+            // 
+            this.lblIRRF.AutoSize = true;
+            this.lblIRRF.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIRRF.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblIRRF.Location = new System.Drawing.Point(62, 67);
+            this.lblIRRF.Name = "lblIRRF";
+            this.lblIRRF.Size = new System.Drawing.Size(0, 20);
+            this.lblIRRF.TabIndex = 12;
+            // 
+            // lblSEST_SENAT
+            // 
+            this.lblSEST_SENAT.AutoSize = true;
+            this.lblSEST_SENAT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSEST_SENAT.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblSEST_SENAT.Location = new System.Drawing.Point(122, 109);
+            this.lblSEST_SENAT.Name = "lblSEST_SENAT";
+            this.lblSEST_SENAT.Size = new System.Drawing.Size(0, 20);
+            this.lblSEST_SENAT.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 232);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(195, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "TOTAL DE IMPOSTOS:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.Color.Red;
+            this.lblTotal.Location = new System.Drawing.Point(210, 228);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 24);
+            this.lblTotal.TabIndex = 15;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSEST_SENAT);
+            this.groupBox1.Controls.Add(this.lblIRRF);
+            this.groupBox1.Controls.Add(this.lblINSS);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(8, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 150);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cálculo dos Impostos";
+            // 
+            // frmCalcTAC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(440, 273);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtValorFrete);
+            this.Controls.Add(this.label4);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmCalcTAC";
+            this.Text = "Calculadora de Impostos TAC v01";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmCalcTAC_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtValorFrete;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label lblINSS;
+        private System.Windows.Forms.Label lblIRRF;
+        private System.Windows.Forms.Label lblSEST_SENAT;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Calc_Impostos_TAC/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Calc_Impostos_TAC
+{
+    public partial class frmCalcTAC : Form
+    {
+        public frmCalcTAC()
+        {
+            InitializeComponent();
+        }
+
+        public void CalcularImpostos()
+        {
+
+            try
+            {
+                double ValorFrete = double.Parse(txtValorFrete.Text);
+                //calculos
+                double baseCalculo = ValorFrete * 0.2;
+                double baseCalculoIRRF = ValorFrete * 0.1;
+                double INSS = baseCalculo * 0.11;
+                double SEST_SENAT = baseCalculo * 0.025;
+
+                double IRRF = 0;
+                if (baseCalculoIRRF < 1903.98)
+                {
+                    IRRF = baseCalculoIRRF * 0;
+                }
+                else if ((baseCalculoIRRF > 1903.98) && (baseCalculoIRRF < 2826.65))
+                {
+                    IRRF = baseCalculoIRRF * 0.075;
+                }
+                else if ((baseCalculoIRRF > 2826.65) && (baseCalculoIRRF < 3751.05))
+                {
+                    IRRF = baseCalculoIRRF * 0.15;
+                }
+                else if ((baseCalculoIRRF > 3751.05) && (baseCalculoIRRF < 4664.68))
+                {
+                    IRRF = baseCalculoIRRF * 0.225;
+                }
+                else
+                {
+                    IRRF = baseCalculoIRRF * 0.275;
+                }
+                //PARA MANTER O VALOR DO INSS NO LIMITE LEGAL
+                if (INSS > 621.04)
+                {
+                    INSS = 621.04;
+                }
+
+                double total = INSS + IRRF + SEST_SENAT;
+
+                lblINSS.Text = INSS.ToString("C");
+                lblIRRF.Text = IRRF.ToString("C");
+                lblSEST_SENAT.Text = SEST_SENAT.ToString("C");
+                lblTotal.Text = total.ToString("C");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show( e.Message , "Erro!");
+            }
+
+        }
+        
+        public void LimparCampos()
+        {
+            txtValorFrete.Text = string.Empty;
+            lblINSS.Text = string.Empty;
+            lblIRRF.Text = string.Empty;
+            lblSEST_SENAT.Text = string.Empty;
+            lblTotal.Text = string.Empty;
+        }
+
+                          
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            CalcularImpostos();
+        }
+                      
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void BtnFechar_Click(object sender, EventArgs e)
+        {
+            
+            if(MessageBox.Show("Tem certeza que deseja sair?", "Confirmação", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+                        
+        }
+        
+        private void TxtValorFrete_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)//Executa o método quando pressionada a tecla Enter quando foco no textBox
+            {
+                CalcularImpostos();
+            }
+            
+        }
+
+        private void FrmCalcTAC_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                
+                if (MessageBox.Show("Tem certeza que deseja sair?", "Confirmação", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
